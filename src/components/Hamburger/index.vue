7c64e2252c543c67637f624a51b1ad180ee6a9ae
kylvia <hh.todd@qq.com>
2019-01-09 16:21:02 +0800

    vue后台模板

--- src/components/Hamburger/index.vue
+++ src/components/Hamburger/index.vue
@@ -25,17 +25,17 @@ export default {
 }
 </script>
 
-<style scoped>
-.hamburger {
-	display: inline-block;
-	cursor: pointer;
-	width: 20px;
-	height: 20px;
-	transform: rotate(90deg);
-	transition: .38s;
-	transform-origin: 50% 50%;
-}
-.hamburger.is-active {
-	transform: rotate(0deg);
-}
+<style rel="stylesheet/less" lang="less" scoped>
+  .hamburger {
+    display: inline-block;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    transform: rotate(90deg);
+    transition: .38s;
+    transform-origin: 50% 50%;
+  }
+  .hamburger.is-active {
+    transform: rotate(0deg);
+  }
 </style>
